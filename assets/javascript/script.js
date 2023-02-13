035b3c094811ee5977eee7d23e4d00c865d592a4
Green Camper <stefan.palacios@gmail.com>
2023-02-12 18:44:06 -0600

    Added functions to create all combination password. Improved for loop to send the correct password to html.

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,7 +1,7 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-//Object for password character values 
+//Global object for password character values 
 var characterBank = {
   lower: ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"],
   upper: ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"],
@@ -33,10 +33,10 @@ function writePassword() {
  
 
   
-
+    //This prompt lets the user confirm the character length of the password and converts this string to a number
     var stringPrompt = prompt("How many characters would you like in your password between 8-128?");
     var passwordLength = Number(stringPrompt);
-
+  //This if statement prompts the user about including certain character sets in the password
   if (passwordLength >= 8 && passwordLength <= 128) {
       var lowerPrompt = confirm("Would you like to include: lowercase, uppercase, numeric, and/or special characters in your password?");
       var upperPrompt = confirm("Would you like to include: uppercase characters in your password?");
@@ -47,36 +47,62 @@ function writePassword() {
   }
 
   console.log(passwordLength);
+  //This function call sends the value of the password length prompt to the generatePassword function
   generatePassword(passwordLength);
 
-
+  //Builds password piece by piece 
   function generatePassword(){
     var functionLength = passwordLength;
     var pass = "";
     console.log(pass);
-
+    //Function for adding lowercase characters to password
     function lowerFunction() {
       console.log(pass);
       pass += characterBank["lower"][Math.floor(Math.random()*characterBank.lower.length)];
       console.log(pass);
       return pass;
     };
-    
-    lowerFunction();
 
-    console.log(functionLength);
-
-    for (i=0 ;i<functionLength; i++){
-      if (lowerPrompt===true){
-        lowerFunction(pass);
-        console.log(pass);
-      }
-      
+    function upperFunction() {
+      console.log(pass);
+      pass += characterBank["upper"][Math.floor(Math.random()*characterBank.upper.length)];
+      console.log(pass);
+      return pass;
     };
 
+    function numericFunction() {
+      console.log(pass);
+      pass += characterBank["numeric"][Math.floor(Math.random()*characterBank.numeric.length)];
+      console.log(pass);
+      return pass;
+    };
+
+    function specialFunction() {
+      console.log(pass);
+      pass += characterBank["special"][Math.floor(Math.random()*characterBank.special.length)];
+      console.log(pass);
+      return pass;
+    };
+
+
+    console.log(functionLength);
+    //For loop for repeatedly adding characters to password
+    for (i=0 ;i<functionLength; i++){
+      if (lowerPrompt&&upperPrompt&&numericPrompt&&specialPrompt){
+        lowerFunction(pass);
+        upperFunction(pass);
+        numericFunction(pass);
+        specialFunction(pass);
+        console.log(pass);
+        pass = pass.substring(0,functionLength)
+      } 
+      
+    };
+    
+    //This returns the final pass string to the password variable
     return pass;
   };
-
+  //This is the starter code that sends the password string out of of the function 
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
   passwordText.value = password;
